48100d06ba6b92eed000b01bcf5acbf90e3e3b0e
chenfengjin <chenfengjin@zhihu.com>
2021-02-28 13:01:49 +0800

    RFC: Add grpc generated files

---
+++ core/contractsdk/python/xuperchain/__init__.py
@@ -0,0 +1,14 @@
+
+
+
+class Contract():
+    def __init__(self):
+        pass
+    def run(self):
+        pass
+    def get_object(self):
+        pass
+    def put_object(self):
+        pass
+
+
